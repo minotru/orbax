8954cb2dc8e3c5d52d4971edce335e1100a32c51
Simon Karasik <sbkarasik@nebius.com>
2024-02-12 19:36:33 +0200

    Apply internal patches from commit 9c0fa6c by max@ aka (@zlobber) to support YT

--- checkpoint/orbax/checkpoint/utils.py
+++ checkpoint/orbax/checkpoint/utils.py
@@ -48,6 +48,7 @@ _AGGREGATED_PREFIX = 'AGGREGATED://'
 _PLACEHOLDER_PREFIX = 'PLACEHOLDER://'
 _COMMIT_SUCCESS_FILE = 'commit_success.txt'
 _GCS_PATH_PREFIX = 'gs://'
+_YT_PATH_PREFIX = 'yt://'
 _LOCK_ITEM_NAME = 'LOCKED'
 _LAST_CHECKPOINT_WRITE_TIME = time.time()
 CheckpointDirs = Tuple[str, str]
@@ -436,6 +437,10 @@ def cleanup_tmp_directories(
 
 def is_gcs_path(path: epath.Path):
   return os.fspath(path).startswith(_GCS_PATH_PREFIX)
+
+
+def is_yt_path(path: epath.Path):
+  return os.fspath(path).startswith(_YT_PATH_PREFIX)
 
 
 def get_tmp_directory(path: epath.Path) -> epath.Path:
